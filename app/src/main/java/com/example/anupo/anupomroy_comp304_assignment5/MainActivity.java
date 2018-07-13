362969ee27fc572873dde18856dbb50b67863ad5
Anupom Roy <anupomr@yahoo.com>
2018-07-13 16:23:24 -0400

    Showroom location functionality created

--- app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/MainActivity.java
+++ app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/MainActivity.java
@@ -6,8 +6,11 @@ package com.example.anupo.anupomroy_comp304_assignment5;
  * Professor:Vinayagathas Vaithilingam
  * Lab:     LabAssignment 5
  * */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,5 +28,38 @@ public class MainActivity extends AppCompatActivity {
         brandArray=getResources().getStringArray(R.array.brand);
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,brandArray);
         listView.setAdapter(arrayAdapter);
+
+        // List View action listener Code
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showroomIntent=null;
+                switch (position)
+                {
+                    case 0:
+                        showroomIntent=new Intent(MainActivity.this,ShowroomsActivity.class);
+                        showroomIntent.putExtra("name","chevrolet");
+                        break;
+                    case 1:
+                        showroomIntent=new Intent(MainActivity.this,ShowroomsActivity.class);
+                        showroomIntent.putExtra("name","ford");
+                        break;
+                    case 2:
+                        showroomIntent=new Intent(MainActivity.this,ShowroomsActivity.class);
+                        showroomIntent.putExtra("name","honda");
+                        break;
+                    case 3:
+                        showroomIntent=new Intent(MainActivity.this,ShowroomsActivity.class);
+                        showroomIntent.putExtra("name","nissan");
+                        break;
+                    case 4:
+                        showroomIntent=new Intent(MainActivity.this,ShowroomsActivity.class);
+                        showroomIntent.putExtra("name","toyota");
+                        break;
+
+                }
+                startActivity(showroomIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/ShowroomsActivity.java
+++ app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/ShowroomsActivity.java
@@ -1,13 +1,46 @@
 package com.example.anupo.anupomroy_comp304_assignment5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ShowroomsActivity extends AppCompatActivity {
+    ListView listView;
+    //declare an array
+    String [] hondaArray={"Scarborough","Downtown","Markham","DonMills"};
+    String []toyotaArray={"Scarborough","Etobicoke","Markham","DonMills"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showrooms);
+
+
+     String name=getIntent().getStringExtra("name");
+        if (name.equals("honda")) {
+            listView=(ListView)findViewById(R.id.showroomListView);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hondaArray);
+            listView.setAdapter(arrayAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    switch (position) {
+                        case 0:
+                            startActivity(new Intent(ShowroomsActivity.this,MapsActivity.class));
+                            break;
+                    }
+
+                }
+            });
+        }
+        if (name.equals("toyota")) {
+            listView=(ListView)findViewById(R.id.showroomListView);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toyotaArray);
+            listView.setAdapter(arrayAdapter);
+        }
     }
 }
