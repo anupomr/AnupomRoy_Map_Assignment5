bb0ff0e3874d930cf6cec3fcfc33093fc3ded1e3
Anupom Roy <anupomr@yahoo.com>
2018-07-13 11:40:34 -0400

    List view functionality created

--- app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/MainActivity.java
+++ app/src/main/java/com/example/anupo/anupomroy_comp304_assignment5/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.anupo.anupomroy_comp304_assignment5;
-
+/*
+ * Author:  Anupom Roy
+ * Subject: Mobile Application Development
+ * date:    July 11, 2018
+ * Professor:Vinayagathas Vaithilingam
+ * Lab:     LabAssignment 5
+ * */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //declare an array
+    String []brandArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //List view Code
+        ListView listView=(ListView)findViewById(R.id.myListView);
+        brandArray=getResources().getStringArray(R.array.brand);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,brandArray);
+        listView.setAdapter(arrayAdapter);
     }
 }
